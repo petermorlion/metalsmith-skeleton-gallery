44a222c6d87fe678db9d90d8d1076230450f300d
Peter Morlion <peter.morlion@live.be>
2015-10-10 13:52:36 +0200

    allow options for columns of skeleton css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function plugin(options){
 
   options = options || {};
   var gallery_root = options.root + '/' || './';
+  var columns = options.columns || 'three';
 
   // return the function to be given to the `.use` call.
   return function(files, metalsmith, done){
@@ -22,7 +23,7 @@ function plugin(options){
       for (var j = 0; j < gallery_files.length; j++) {
         var gallery_file = gallery_files[j].trim();
         var file_url = gallery_root + gallery_file;
-        gallery_html_parts.push('<div class="four columns"><img src="' + file_url + '" /></div>');
+        gallery_html_parts.push('<div class="' + columns + ' columns"><img src="' + file_url + '" style="width: 100%;" /></div>');
       }
 
       data.skeletongallery = gallery_html_parts.join(" ");
