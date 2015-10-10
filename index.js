1f7530490b8bfd196379b77dcb0f85b748e16768
Peter Morlion <peter.morlion@live.be>
2015-10-10 13:44:51 +0200

    working index.js

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var skeletongallery = function(options){
+module.exports = plugin;
+
+function plugin(options){
 
   // commonly set default options here
 
   // return the function to be given to the `.use` call.
   return function(files, metalsmith, done){
+    setImmediate(done);
+    Object.keys(files).forEach(function(file){
 
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
+      var data = files[file];
 
-      if(!file.skeletongallery) {
-        continue;
+      if(!data.skeletongallery) {
+        return;
       }
 
-      var gallery_files = file.skeletongallery.split(',');
-      for (var j = 0; i < gallery_files.length; j++) {
-        var gallery_file = gallery_files[i].trim();
+      var gallery_html_parts = [];
 
-        file.skeletongallery = gallery_file;
+      var gallery_files = data.skeletongallery.split(',');
+      for (var j = 0; j < gallery_files.length; j++) {
+        var gallery_file = gallery_files[j].trim();
+        gallery_html_parts.push('<div class="four columns"><img src="' + gallery_file + '" /></div>');
       }
-    }
 
-    done();
+      data.skeletongallery = gallery_html_parts.join(" ");
+    });
   };
-};
+}
